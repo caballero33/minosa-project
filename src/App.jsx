c0d4aeb2042e299ee696444fadb1184e9d6e068e
caballero33 <josephalcerro33@gmail.com>
2025-09-20 18:51:32 -0600

    Primer commit del proyecto

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Minosa Project</h1>
+      <p>Base React project lista para desarrollar</p>
+    </div>
+  )
+}
+
+export default App
